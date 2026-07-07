3bcbe42cad938957743eb5bf285a3f4e0e39192b
Paul Rodriguez <paulyd0123@gmail.com>
2026-07-07 18:45:54 -0400

    Initial commit

---
+++ anchorTests/anchorTests.swift
@@ -0,0 +1,18 @@
+//
+//  anchorTests.swift
+//  anchorTests
+//
+//  Created by Paul Rodriguez on 7/7/26.
+//
+
+import Testing
+
+struct anchorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
